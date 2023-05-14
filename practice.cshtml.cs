f5aa9b3f504aab00a7e9c2825bf082abba05482f
dpmanGH <132672313+dpmanGH@users.noreply.github.com>
2023-05-14 12:14:46 +0600

    Default Folder

---
+++ practice.cshtml.cs
@@ -0,0 +1,21 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Demo.Pages
+{
+    public class practiceModel : PageModel
+    {
+        public bool isok = false;
+        public String a = "";
+        public String b = "";
+        public void OnGet()
+        {
+        }
+        public void OnPost()
+        {
+            isok = true;
+            a = Request.Form["a"];
+            b = Request.Form["b"];
+        }
+    }
+}
